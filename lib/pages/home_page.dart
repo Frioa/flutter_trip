297e48f2ddb7f6dc37ac55950c436cdcd4c42d78
Frioa <916378508@qq.com>
2019-06-12 18:26:27 +0800

    1. 滚动渐变效果，使用了ListView，去掉ListView的PaddingTop， 2. NotificationListener 监听列表滚动，通过onNotification 方法回调，会检查它子child元素的滚动情况，设置depth监听的深度。 3.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+const APPBAR_SCROLL_OFFSET = 110;
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -12,31 +13,74 @@ class _HomePageState extends State<HomePage> {
     'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg',
     'https://dimg04.c-ctrip.com/images/700c10000000pdili7D8B_780_235_57.jpg'
   ];
+  var appBarAlpha = 0.0;
+  _onScroll(offset) {
+    var alpha = offset / APPBAR_SCROLL_OFFSET;
+    if(alpha < 0) {
+      alpha = 0;
+    }else if(alpha > 1) {
+      alpha = 1;
+    }
+    setState(() {
+      appBarAlpha = alpha;
+    });
+    print(appBarAlpha);
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 160,
-                child: Swiper(
-                  itemCount: _imageUrls.length,
-                  autoplay: true,
-                  itemBuilder: (BuildContext context, int index){
-                    return Image.network(
-                      _imageUrls[index],
-                      fit: BoxFit.fill
-                    );
-                  },
-                  pagination: SwiperPagination(),
+        body: Stack(
+      children: <Widget>[
+        MediaQuery.removePadding(
+          removeTop: true, // 去掉顶部屏幕适配
+          context: context,
+          child: NotificationListener(
+            onNotification: (scrollNotification) {
+              if (scrollNotification is ScrollUpdateNotification &&
+                  scrollNotification.depth == 0) {
+                // 优化, 并且只监听ListView的滚动
+                // 滚动且是列表滚动的时候
+                _onScroll(scrollNotification.metrics.pixels);
+              }
+            },
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  height: 160,
+                  child: Swiper(
+                    itemCount: _imageUrls.length,
+                    autoplay: true,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Image.network(_imageUrls[index], fit: BoxFit.fill);
+                    },
+                    pagination: SwiperPagination(), // 指示器
+                  ),
                 ),
-              )
-            ],
+                Container(
+                  height: 800,
+                  child: ListTile(
+                    title: Text("haha"),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
-    );
+        Opacity(
+          opacity: appBarAlpha,
+          child: Container(
+            height: 80,
+            decoration: BoxDecoration(color: Colors.white),
+            child: Center(
+              child: Padding(padding: EdgeInsets.only(top: 20),
+                  child: Text('首页'),
+              ),
+            ),
+          ),
+        )
+      ],
+    ));
   }
-
 }
