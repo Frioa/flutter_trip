c3aeb68ab99371f35f53d5bc52d9f43bcbbf840c
Frioa <916378508@qq.com>
2019-06-13 20:12:00 +0800

    注释添加

--- lib/model/home_model.dart
+++ lib/model/home_model.dart
@@ -19,9 +19,10 @@ class HomeModel {
       this.gridNav,
       this.salesBox});
 
+  // 反序列化
   factory HomeModel.fromJson(Map<String, dynamic> json) {
-    var localNavListJson = json['localNavList'] as List;
-    List<CommonModel> localNavList =
+    var localNavListJson = json['localNavList'] as List;// 转化为字符串数组
+    List<CommonModel> localNavList =//每个字符串进行fromJson
         localNavListJson.map((i) => CommonModel.fromJson(i)).toList();
 
     var bannerListJson = json['bannerList'] as List;
@@ -42,6 +43,7 @@ class HomeModel {
     );
   }
 
+  // 序列化
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['config'] = this.config;
