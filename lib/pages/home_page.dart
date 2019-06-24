47081e4e0d47e4dc5961d228f1a8f7557e282b19
Frioa <916378508@qq.com>
2019-06-24 19:47:43 +0800

    搜索页面Item显示 Item布局的设计， 首页输入框跳转到搜索页，跳转函数的实现

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_trip/model/grid_nav_model.dart';
 
 import 'package:flutter_trip/model/home_model.dart';
 import 'package:flutter_trip/model/sales_box_model.dart';
+import 'package:flutter_trip/pages/search_page.dart';
 import 'package:flutter_trip/widget/grid_nav.dart';
 import 'package:flutter_trip/widget/local_nav.dart';
 import 'package:flutter_trip/widget/loding_container.dart';
@@ -16,19 +17,23 @@ import 'package:flutter_trip/widget/webview.dart';
 
 const APPBAR_SCROLL_OFFSET = 110;
 const SEARCH_BAR_DEFAULT_TEXT = '网红打卡地 景点 酒店 美食';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   var appBarAlpha = 0.0;
   String resultString = "resultString";
-  List<CommonModel> localNavList = [] ;
-  List<CommonModel> bannerList = [] ;
-  List<CommonModel> subNavList = [] ;
-  GridNavModel gridNavModel  ;
+  List<CommonModel> localNavList = [];
+
+  List<CommonModel> bannerList = [];
+
+  List<CommonModel> subNavList = [];
+
+  GridNavModel gridNavModel;
+
   SalesBoxModel salesBoxModel;
   bool _loading = true;
 
@@ -52,7 +57,6 @@ class _HomePageState extends State<HomePage> {
     print(appBarAlpha);
   }
 
-
   Future<Null> _handleRefresh() async {
     try {
       HomeModel mode = await HomeDao.fetch();
@@ -64,7 +68,7 @@ class _HomePageState extends State<HomePage> {
         bannerList = mode.bannerList;
         _loading = false;
       });
-    }catch (e){
+    } catch (e) {
       print(e);
       _loading = false;
     }
@@ -74,52 +78,60 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xfff2f2f2),
-        body: LoadingContainer(isLoading: _loading,
+        backgroundColor: Color(0xfff2f2f2),
+        body: LoadingContainer(
+            isLoading: _loading,
             child: Stack(
-          children: <Widget>[
-            MediaQuery.removePadding(
-              // 去掉顶部屏幕适配
-              removeTop: true,
-              context: context,
-              child: RefreshIndicator(
-                  child: NotificationListener(
-                onNotification: (scrollNotification) {
-                  if (scrollNotification is ScrollUpdateNotification &&
-                      scrollNotification.depth == 0) {
-                    // 优化, 防止scrollNotification=0的时候也监听，只有在ScrollUpdateNotification更新的时候才监听并且只监听ListView的滚动滚动且是列表滚动的时候
-                    _onScroll(scrollNotification.metrics.pixels);
-                  }
-                },
-                child: _listView,
-              ),
-                  onRefresh: _handleRefresh),
-            ),
-            _appBar,
-          ],
-        )
-        )
-    );
+              children: <Widget>[
+                MediaQuery.removePadding(
+                  // 去掉顶部屏幕适配
+                  removeTop: true,
+                  context: context,
+                  child: RefreshIndicator(
+                      child: NotificationListener(
+                        onNotification: (scrollNotification) {
+                          if (scrollNotification is ScrollUpdateNotification &&
+                              scrollNotification.depth == 0) {
+                            // 优化, 防止scrollNotification=0的时候也监听，只有在ScrollUpdateNotification更新的时候才监听并且只监听ListView的滚动滚动且是列表滚动的时候
+                            _onScroll(scrollNotification.metrics.pixels);
+                          }
+                        },
+                        child: _listView,
+                      ),
+                      onRefresh: _handleRefresh),
+                ),
+                _appBar,
+              ],
+            )));
   }
+
   Widget get _listView {
     return ListView(
       children: <Widget>[
         _banner,
         Padding(
           padding: EdgeInsets.fromLTRB(7, 4, 7, 4), // 四周的Padding
-          child:  LocalNav(localNavList: localNavList,),
+          child: LocalNav(
+            localNavList: localNavList,
+          ),
         ),
         Padding(
           padding: EdgeInsets.fromLTRB(7, 0, 7, 4), // 四周的Padding
-          child:  GridNav(gridNavModel: gridNavModel,),
+          child: GridNav(
+            gridNavModel: gridNavModel,
+          ),
         ),
         Padding(
           padding: EdgeInsets.fromLTRB(7, 0, 7, 4), // 四周的Padding
-          child:  SubNav(subNavList: subNavList,),
+          child: SubNav(
+            subNavList: subNavList,
+          ),
         ),
         Padding(
           padding: EdgeInsets.fromLTRB(7, 0, 7, 4), // 四周的Padding
-          child:  SalesBox(salesBox: salesBoxModel,),
+          child: SalesBox(
+            salesBox: salesBoxModel,
+          ),
         ),
         Container(
           height: 400,
@@ -130,24 +142,25 @@ class _HomePageState extends State<HomePage> {
       ],
     );
   }
+
   Widget get _appBar {
-    return Column(// 上面输入，下面阴影
+    return Column(
+      // 上面输入，下面阴影
       children: <Widget>[
         Container(
           decoration: BoxDecoration(
-            gradient: LinearGradient(
-              // AppBar 渐变遮罩背景, 由透明到
-              colors: [Color(0x66000000), Colors.transparent],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            )
-          ),
+              gradient: LinearGradient(
+            // AppBar 渐变遮罩背景, 由透明到
+            colors: [Color(0x66000000), Colors.transparent],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          )),
           child: Container(
             padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
             height: 80.0,
             decoration: BoxDecoration(
-              color: Color.fromARGB((appBarAlpha * 255).toInt(), 255, 255, 255)
-            ),
+                color:
+                    Color.fromARGB((appBarAlpha * 255).toInt(), 255, 255, 255)),
             child: SearchBar(
               searchBarType: appBarAlpha > 0.2
                   ? SearchBarType.homeLight
@@ -155,24 +168,21 @@ class _HomePageState extends State<HomePage> {
               inputBoxClick: _jumpToSearch,
               speakClick: _jumpToSpeak,
               defaultText: SEARCH_BAR_DEFAULT_TEXT,
-              leftButtonClick: () {
-              },
+              leftButtonClick: () {},
             ),
           ),
         ),
-        Container(// 阴影设置
+        Container(
+          // 阴影设置
           height: appBarAlpha > 0.2 ? 0.5 : 0,
           decoration: BoxDecoration(
-            boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 0.5)]
-          ),
+              boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 0.5)]),
         )
       ],
     );
-
-
-
   }
-  Widget get _banner{
+
+  Widget get _banner {
     return Container(
       height: 160,
       child: Swiper(
@@ -193,20 +203,21 @@ class _HomePageState extends State<HomePage> {
                 }),
               );
             },
-            child: Image.network(
-                bannerList[index].icon,
-                fit: BoxFit.fill
-            ),
+            child: Image.network(bannerList[index].icon, fit: BoxFit.fill),
           );
         },
         pagination: SwiperPagination(), // 指示器
       ),
     );
   }
+
   void _jumpToSearch() {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return SearchPage(
+        hint: SEARCH_BAR_DEFAULT_TEXT,
+      );
+    }));
   }
 
-  void _jumpToSpeak() {
-  }
-
+  void _jumpToSpeak() {}
 }
--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_trip/dao/search_dao.dart';
 import 'package:flutter_trip/model/search_model.dart';
 import 'package:flutter_trip/widget/search_bar.dart';
+import 'package:flutter_trip/widget/webview.dart';
 
 const URL =
     'https://m.ctrip.com/restapi/h5api/searchapp/search?source=mobileweb&action=autocomplete&contentType=json&keyword=';
@@ -9,7 +10,7 @@ const URL =
 class SearchPage extends StatefulWidget {
   final bool hideLeft;
   final String searchUrl;
-  final String keyword;// 判断搜索关键字是否与结果对应
+  final String keyword; // 判断搜索关键字是否与结果对应
   final String hint;
 
   const SearchPage(
@@ -32,18 +33,19 @@ class _SearchPageState extends State<SearchPage> {
         children: <Widget>[
           _appBar(),
           // 通过ListView.builder创建ListView 效率高
-         MediaQuery.removePadding(// 移除ListView顶部空白
-           removeTop: true,
-             context: context,
-             child:  Expanded(
-           // ListView 填充剩余的高度
-           flex: 1,
-           child: ListView.builder(
-               itemCount: searchModel?.data?.length ?? 0,
-               itemBuilder: (BuildContext context, int position) {
-                 return _item(position);
-               }),
-         )),
+          MediaQuery.removePadding(
+              // 移除ListView顶部空白
+              removeTop: true,
+              context: context,
+              child: Expanded(
+                // ListView 填充剩余的高度
+                flex: 1,
+                child: ListView.builder(
+                    itemCount: searchModel?.data?.length ?? 0,
+                    itemBuilder: (BuildContext context, int position) {
+                      return _item(position);
+                    }),
+              )),
         ],
       ),
     );
@@ -60,7 +62,7 @@ class _SearchPageState extends State<SearchPage> {
     String url = widget.searchUrl + text;
     SearchDao.fetch(url, text).then((SearchModel model) {
       // 只有当当前输入的内容与服务端返回的内容一致才渲染
-      if(model.keyword == keyword){
+      if (model.keyword == keyword) {
         setState(() {
           searchModel = model;
         });
@@ -102,6 +104,33 @@ class _SearchPageState extends State<SearchPage> {
   _item(int position) {
     if (searchModel == null || searchModel.data == null) return null;
     SearchItem item = searchModel.data[position];
-    return Text(item.word);
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => WebView(
+                      url: item.url,
+                      title: '详情',
+                    )));
+      },
+      child: Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            border: Border(
+                bottom: BorderSide(width: 0.3, color: Colors.grey)) // 底部的线
+            ),
+        child: Row(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Container(width: 300, child: Text('${item.word} ${item.districtname?? ''} ${item.zonename?? ''}'),),
+                Container(width: 300, child: Text('${item.price} ${item.type?? ''} '),)
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
