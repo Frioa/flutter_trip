a00565e7103d9011bec6a53cdeeae784d6b74359
Frioa <916378508@qq.com>
2019-06-24 16:28:40 +0800

    SearchModel 的封装 SearchDao 的实现

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,31 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_trip/dao/search_dao.dart';
+import 'package:flutter_trip/model/search_model.dart';
 import 'package:flutter_trip/widget/search_bar.dart';
 
-class SearchPage extends StatefulWidget{
+class SearchPage extends StatefulWidget {
   @override
   _SearchPageState createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> {
-
+  String showText = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-        body: Column(
-         children: <Widget>[
-           SearchBar(
-             hideLeft: true,
-               defaultText: 'haha',
-             hint: '123',
-             leftButtonClick: (){
-               Navigator.pop(context);
-             },
-             onChanged: _onTextChange,
-           ),
-         ],
-        ),
+      body: Column(
+        children: <Widget>[
+          SearchBar(
+            hideLeft: true,
+            defaultText: 'haha',
+            hint: '123',
+            leftButtonClick: () {
+              Navigator.pop(context);
+            },
+            onChanged: _onTextChange,
+          ),
+          InkWell(
+            onTap: () {
+              SearchDao.fetch(
+                  'https://m.ctrip.com/restapi/h5api/searchapp/search?source=mobileweb&action=autocomplete&contentType=json&keyword=长城')
+                  .then((SearchModel value) {
+                setState(() {
+                  showText = value.data[0].url;
+                });
+              });
+            },
+            child: Text('Get'),
+          ),
+          Text(showText)
+        ],
+      ),
     );
   }
 
