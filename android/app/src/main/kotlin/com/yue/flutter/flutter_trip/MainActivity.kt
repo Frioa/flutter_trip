cbf18437beca62b3f3ddbc3e492143a036366517
Frioa <916378508@qq.com>
2019-06-26 23:50:37 +0800

    百度SDK接入 1. AsrManager 的实现 2. AsrPlugin 的实现，使用MethodChannel通讯机制， 3. ResultStateful ，MethodChannel 是一次性通信，Dart调用Native与Native调用Dart一次性通信，防止连续调用语音程序崩溃，通过called标识位保证回调一次 3. 自定义Plugin的注册

--- android/app/src/main/kotlin/com/yue/flutter/flutter_trip/MainActivity.kt
+++ android/app/src/main/kotlin/com/yue/flutter/flutter_trip/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.yue.flutter.flutter_trip
 
 import android.os.Bundle
+import com.yue.flutter.plugin.asr.AsrPlugin
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
@@ -9,5 +10,11 @@ class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+    registerSelfPlugin()
   }
+  // 自己插件的注册。
+  private fun registerSelfPlugin() {
+    AsrPlugin.registerWith(registrarFor("com.yue.flutter.plugin.asr.AsrPlugin"))
+  }
+
 }
